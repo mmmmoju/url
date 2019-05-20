1a92deb9828d90cc22bd764378d346c30891500e
mmmmoju <50597545+mmmmoju@users.noreply.github.com>
2019-05-20 13:41:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ console.log("Scrpit By Dream");
 client.on("ready", () => {
 let channel =     client.channels.get("579978587831009281")
 setInterval(function() {
-channel.send(`ilove you moody and braahim`);
+channel.send(`ilove you moody and braahim @«MDX»#0567  @«HMX»`#0039 `);
 }, 30)
 })
 
